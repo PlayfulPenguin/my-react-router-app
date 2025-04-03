90c354ae37522661bf2ed594b628f3b1968c4185
PlayfulPenguin <rachellerqgu@gmail.com>
2025-04-03 13:03:01 -0400

    init commit into git

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,12 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
-
+import { gb } from "../Guestbook/guestbook";
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
+    { title: "you suck" },
     { name: "description", content: "Welcome to React Router!" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return < gb />;
 }
